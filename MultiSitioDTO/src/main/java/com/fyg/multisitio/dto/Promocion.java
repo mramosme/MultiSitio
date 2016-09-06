2584610309bec717d8b4fd289af50e04b8d4de13
RobertoCoto <roberto.coto@fygsolutions.com>
2016-09-06 13:52:46 -0500

    se añade en dto Promocion

---
+++ MultiSitioDTO/src/main/java/com/fyg/multisitio/dto/Promocion.java
@@ -0,0 +1,19 @@
+package com.fyg.multisitio.dto;
+
+import com.fyg.multisitio.comun.ObjetoValor;
+
+/**
+* Clase Promocion
+* Define el catalogo de promociones
+* @author Administrador
+* @version 1.0
+* @created 21-jul-2016 12:38:33 p.m.
+*/
+public class Promocion extends ObjetoValor {
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 7525397544198367754L;
+
+}
