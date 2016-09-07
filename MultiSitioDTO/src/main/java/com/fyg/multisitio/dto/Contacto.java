b94b37ebe5fb0b79d52165cd2ceec77b7fa1f66e
RobertoCoto <roberto.coto@fygsolutions.com>
2016-09-07 10:32:25 -0500

    correcciones checkstyle

--- MultiSitioDTO/src/main/java/com/fyg/multisitio/dto/Contacto.java
+++ MultiSitioDTO/src/main/java/com/fyg/multisitio/dto/Contacto.java
@@ -4,7 +4,7 @@ import com.fyg.multisitio.comun.ObjetoValor;
 
 /**
  * Contactos
- * 
+ *
  * @author Administrador
  * @version 1.0
  * @created 21-jul-2016 12:38:09 p.m.
@@ -12,15 +12,15 @@ import com.fyg.multisitio.comun.ObjetoValor;
 public class Contacto extends ObjetoValor {
 
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 8163048128392157838L;
 	/**
 	 * Identificador unico de registro
-	 *        
+	 *
 	 */
 	private int id;
-	
+
 	/**
 	 * Dato que almacena la calle del contacto
 	 */
@@ -45,52 +45,87 @@ public class Contacto extends ObjetoValor {
 	 * Dato que almacena el estatus
 	 */
 	private int estatus;
-	
+	/**
+	 * @return , obtiene el id unico de registro
+	 */
 	public int getId() {
 		return id;
 	}
-	
+	/**
+	 * @param id ,ingresa el id unico
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
+	/**
+	 * @return ,obtiene la calle
+	 */
 	public String getCalle() {
 		return calle;
 	}
-	
+	/**
+	 * @return ,obtiene el estatus
+	 */
 	public int getEstatus() {
 		 return estatus;
 	}
-	
+	/**
+	 * @param estatus , ingresa un estatus
+	 */
 	public void setEstatus(int estatus) {
 
 	}
-	
+	/**
+	 * @return ,Se obtiene el nombre
+	 */
 	public String getNombre() {
 		return nombre;
 	}
-	
+	/**
+	 * @param calle ,ingresa una calle
+	 */
 	public void setCalle(String calle) {
 		this.calle = calle;
 	}
+	/**
+	 * @return ,obtiene la colonia
+	 */
 	public String getColonia() {
 		return colonia;
 	}
+	/**
+	 * @param colonia ,ingresa una colonia
+	 */
 	public void setColonia(String colonia) {
 		this.colonia = colonia;
 	}
+	/**
+	 * @return ,obtiene un numero de telefono
+	 */
 	public String getTelefono() {
 		return telefono;
 	}
+	/**
+	 * @param telefono ,ingresa un numero de telefono
+	 */
 	public void setTelefono(String telefono) {
 		this.telefono = telefono;
 	}
+	/**
+	 * @return ,obtiene un correo
+	 */
 	public String getCorreo() {
 		return correo;
 	}
+	/**
+	 * @param correo ,ingresa un correo
+	 */
 	public void setCorreo(String correo) {
 		this.correo = correo;
 	}
-	
+	/**
+	 * @param nombre ,ingresa un nombre
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
