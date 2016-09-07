c5dc9266802873b9db5c3967fd91b8cac1ba43dc
RobertoCoto <roberto.coto@fygsolutions.com>
2016-09-07 10:13:41 -0500

    correcciones Checkstyle

--- MultiSitioNegocio/src/test/java/com/fyg/multisitio/negocio/test/SitioTest.java
+++ MultiSitioNegocio/src/test/java/com/fyg/multisitio/negocio/test/SitioTest.java
@@ -12,16 +12,30 @@ import com.fyg.multisitio.comun.LogHandler;
 
 
 public class SitioTest {
+	/**
+	 * Se crea el objeto data para mandarle valores a negocio
+	 */
 	private OperacionesMultiSitioNegocio data;
+	/**
+	 * Se crea objeto sitio para mandar valores
+	 */
 	private Sitio    registraSitio;
+	/**
+	 * Se crea objeto contacto para mandar valores
+	 */
 	private Contacto registroContacto;
+	/**
+	 *Uid unico generado
+	 */
 	 private GUIDGenerator uid;
-	 
-	 
+	 /**
+	  *
+	  * @throws Exception
+	  */
 	@Before
 	public void setUp() throws Exception {
 		 data = new OperacionesMultiSitioNegocio();
-		 
+
 		    /**
 		    * Datos que se enviaran a contactos
 		    */
@@ -31,31 +45,34 @@ public class SitioTest {
 	       registroContacto.setColonia("Polanco");
 	       registroContacto.setCorreo("puebloazul@hotmail.com");
 	       registroContacto.setTelefono("5578902232");
-		 
-		   Date Fecha = new Date();
+
+		   Date fecha = new Date();
 		   registraSitio = new Sitio();
 		   registraSitio.setObjetoContacto(registroContacto);
 		   registraSitio.setNombre("Valle de Bravo");
 		   registraSitio.setDomicilio("ESTADO DE MEXICO");
 		   registraSitio.setTelefono("5567384774");
-		   registraSitio.setFechaRegistro(Fecha);
+		   registraSitio.setFechaRegistro(fecha);
 		   registraSitio.setEstatus(1);
 		   registraSitio.setIdZona(1);
-		
-	}
 
+	}
+	/**
+	 *
+	 * @throws Exception
+	 */
 	@SuppressWarnings("static-access")
 	@Test
 	public void testRegistraSitio() throws Exception {
 		String guid = uid.generateGUID(data);
-		try{
+		try {
 			data.registraSitio(registraSitio);
 			LogHandler.debug(guid,this.getClass(), "PRUEBA JUNIT");
 		}
-		catch(Exception e){
-			
+		catch (Exception ex) {
+			LogHandler.debug(guid,this.getClass(), "Error");
 		}
-		
+
 	}
 
 }
