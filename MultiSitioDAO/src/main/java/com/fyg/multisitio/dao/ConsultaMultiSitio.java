021788f4551a210d6a882c5b8224832f01e96282
RobertoCoto <roberto.coto@fygsolutions.com>
2016-09-08 11:18:39 -0500

    se estara actualizando el fichero

--- MultiSitioDAO/src/main/java/com/fyg/multisitio/dao/ConsultaMultiSitio.java
+++ MultiSitioDAO/src/main/java/com/fyg/multisitio/dao/ConsultaMultiSitio.java
@@ -1,5 +1,5 @@
 package com.fyg.multisitio.dao;
 
 public class ConsultaMultiSitio {
-//Se esta agregando las consultas conforme la interfaz, se estara actualizando
+//Se esta agregando las consultas conforme la interfaz, se estara actualizando el fichero
 }
