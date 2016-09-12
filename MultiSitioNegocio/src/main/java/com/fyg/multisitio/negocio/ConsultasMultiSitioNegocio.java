977a5ede4601bc7ec14f040fa0e656dde136504b
RobertoCoto <roberto.coto@fygsolutions.com>
2016-09-12 17:53:41 -0500

    Se actualiza capa negocio metodo consultas

--- MultiSitioNegocio/src/main/java/com/fyg/multisitio/negocio/ConsultasMultiSitioNegocio.java
+++ MultiSitioNegocio/src/main/java/com/fyg/multisitio/negocio/ConsultasMultiSitioNegocio.java
@@ -1,11 +1,13 @@
 package com.fyg.multisitio.negocio;
 
+import java.util.Iterator;
 import java.util.List;
 
 import com.fyg.multisitio.comun.EncabezadoRespuesta;
 import com.fyg.multisitio.comun.GUIDGenerator;
 import com.fyg.multisitio.comun.LogHandler;
-import com.fyg.multisitio.dto.Negocio;
+import com.fyg.multisitio.dto.FiltroNegocio;
+import com.fyg.multisitio.dto.FiltroSitio;
 import com.fyg.multisitio.dao.ConsultaMultiSitio;
 public class ConsultasMultiSitioNegocio {
 
@@ -14,7 +16,7 @@ public class ConsultasMultiSitioNegocio {
 	 * @param negocio ,Recibe valores de negocio
 	 * @return ,regresa la lista de negocio
 	 */
-	public EncabezadoRespuesta consultaNegocio(Negocio negocio) {
+	public EncabezadoRespuesta consultaNegocio(FiltroNegocio negocio) {
 		//Primero generamos el identificador unico de la transaccion
 		String uid = GUIDGenerator.generateGUID(negocio);
 		//Mandamos a log el objeto de entrada
@@ -22,8 +24,10 @@ public class ConsultasMultiSitioNegocio {
 		//Variable de resultado
 	    EncabezadoRespuesta respuesta = new EncabezadoRespuesta();
 	    try {
-	    	List<String> data = new ConsultaMultiSitio().consultaNegocio(uid, negocio);
-
+	    	List<FiltroNegocio> data = new ConsultaMultiSitio().consultaNegocio(uid, negocio);
+	    	Iterator<FiltroNegocio> iter = data.iterator();
+			while (iter.hasNext())
+			{ System.out.println(iter.next()); }
 	    } catch (Exception ex) {
 	    	LogHandler.error(uid, this.getClass(), "ConsultaMultiSitio - ErrorMultisitio: " + ex.getMessage(), ex);
 			respuesta.setUid(uid);
@@ -31,7 +35,34 @@ public class ConsultasMultiSitioNegocio {
 			respuesta.setMensajeFuncional(ex.getMessage());
 			respuesta.setMensajeTecnico(ex.getMessage());
 	    }
-	    LogHandler.debug(uid, this.getClass(), "consultaNegocio - Daton Salida: " + respuesta);
+	    LogHandler.debug(uid, this.getClass(), "consultaNegocio - Datos Salida: " + respuesta);
+		return respuesta;
+	}
+/**
+ * Metodo que consulta los sitios disponibles
+ * @param sitio ,Recibe valores de sitio
+ * @return ,Regresa una lista sitio
+ */
+	public EncabezadoRespuesta consultaSitio(FiltroSitio sitio) {
+		//Primero generamos el identificador unico de la transaccion
+				String uid = GUIDGenerator.generateGUID(sitio);
+				//Mandamos a log el objeto de entrada
+				LogHandler.debug(uid, this.getClass(), "consultaSitio - Daton Entrada: " + sitio);
+				//Variable de resultado
+			    EncabezadoRespuesta respuesta = new EncabezadoRespuesta();
+			    try {
+			    	List<FiltroSitio> dataSitio = new ConsultaMultiSitio().consultaSitio(uid, sitio);
+			    	Iterator<FiltroSitio> iter = dataSitio.iterator();
+					while (iter.hasNext())
+					{ System.out.println(iter.next()); }
+			    } catch (Exception ex) {
+			    	LogHandler.error(uid, this.getClass(), "ConsultaMultiSitio - ErrorMultisitio: " + ex.getMessage(), ex);
+					respuesta.setUid(uid);
+					respuesta.setEstatus(false);
+					respuesta.setMensajeFuncional(ex.getMessage());
+					respuesta.setMensajeTecnico(ex.getMessage());
+			    }
+			    LogHandler.debug(uid, this.getClass(), "consultaNegocio - Datos Salida: " + respuesta);
 		return respuesta;
 	}
 }
--- MultiSitioNegocio/src/test/java/com/fyg/multisitio/negocio/test/ConsultaTest.java
+++ MultiSitioNegocio/src/test/java/com/fyg/multisitio/negocio/test/ConsultaTest.java
@@ -7,17 +7,18 @@ import org.junit.Test;
 
 import com.fyg.multisitio.negocio.ConsultasMultiSitioNegocio;
 import com.fyg.multisitio.comun.LogHandler;
+import com.fyg.multisitio.dto.FiltroNegocio;
 import com.fyg.multisitio.dto.Negocio;
 
 public class ConsultaTest {
 	private ConsultasMultiSitioNegocio data;
-	private Negocio dataNegocio;
+	private FiltroNegocio dataNegocio;
 	
 	@Before
 	public void setUp() throws Exception {
 		data = new ConsultasMultiSitioNegocio();
-		dataNegocio = new Negocio();
-		dataNegocio.setId(3);
+		dataNegocio = new FiltroNegocio();
+		dataNegocio.setIdNegocio(3);
 	}
 
 	@Test
