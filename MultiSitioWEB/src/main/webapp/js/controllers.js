361d002846d26e25681fa9c8cbc5105518513537
Roberto Coto <robertocoto@fygsolutions.com>
2016-09-15 11:27:10 -0500

    se actualizan archivos de angular js maven

--- MultiSitioWEB/src/main/webapp/js/controllers.js
+++ MultiSitioWEB/src/main/webapp/js/controllers.js
@@ -1,7 +1,15 @@
-'use strict';
-
 var controllers = angular.module("controllers", []);
 
-controllers.controller("HomeController", ['$scope', function ($scope) {
-   $scope.title = 'Hello world!';
+controllers.controller("HomeController", ['$scope', '$http', function ($scope, $http) {
+    $scope.debug = false;
+    $scope.title = 'Hello ';
+
+    $http.get("service/home").success(function (data) {
+        $scope.data = data;
+        $scope.title += $scope.data.message;
+    });
+
+    $scope.toggleDebug = function () {
+        $scope.debug = !$scope.debug;
+    };
 }]);
