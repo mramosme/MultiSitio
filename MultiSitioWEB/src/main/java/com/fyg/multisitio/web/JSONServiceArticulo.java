f3e77d3784e2dd8396577d86b4b89973385c0229
RobertoCoto <roberto.coto@fygsolutions.com>
2016-09-29 15:09:13 -0500

    se actualiza JSON

--- MultiSitioWEB/src/main/java/com/fyg/multisitio/web/JSONServiceArticulo.java
+++ MultiSitioWEB/src/main/java/com/fyg/multisitio/web/JSONServiceArticulo.java
@@ -1,5 +1,8 @@
 package com.fyg.multisitio.web;
 
+import java.util.Date;
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -12,6 +15,7 @@ import org.json.JSONObject;
 import com.fyg.multisitio.negocio.ConsultasMultiSitioNegocio;
 import com.fyg.multisitio.dto.Articulo;
 
+
 @Path("/articulo")
 public class JSONServiceArticulo {
 	
@@ -19,8 +23,25 @@ public class JSONServiceArticulo {
 	  @GET
 	  @Produces("application/json")
 	  public Response jsonArticulo (@PathParam("i") Integer f) throws JSONException {
+		 
 		  JSONObject jsonObject = new JSONObject();
+		  Articulo idArticulo = new Articulo();
+		  idArticulo.setId(f);
 		  
+		  List<Articulo> lista = new ConsultasMultiSitioNegocio().consultaArticulo(idArticulo);
+		  for(int i = 0; i < lista.size(); i++) {
+			  String nombre  = lista.get(i).getNombre();
+			  String descripcion = lista.get(i).getDescripcion();
+			  Double precio = lista.get(i).getPrecio();
+			  Date fechaRegistro = lista.get(i).getFechaRegistro();
+			  int estatus = lista.get(i).getEstatus();
+			  
+			  jsonObject.put("nombreArticulo", nombre);
+			  jsonObject.put("descripcion", descripcion);
+			  jsonObject.put("precio", precio);
+			  jsonObject.put("fechaRegistro", fechaRegistro);
+			  jsonObject.put("estatus", estatus);
+		  }
 		  
 		    String result = "" + jsonObject;
 			return Response.status(200).entity(result).build();
