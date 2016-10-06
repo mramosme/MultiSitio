30abf7823aed6ab6435505fa95483767c4546df2
RobertoCoto <roberto.coto@fygsolutions.com>
2016-10-06 10:32:34 -0500

    1era version test actividad

---
+++ MultiSitioNegocio/src/test/java/com/fyg/multisitio/negocio/test/ActividadTest.java
@@ -0,0 +1,19 @@
+package com.fyg.multisitio.negocio.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ActividadTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testRegistraActividad() {
+		fail("Not yet implemented");
+	}
+
+}
