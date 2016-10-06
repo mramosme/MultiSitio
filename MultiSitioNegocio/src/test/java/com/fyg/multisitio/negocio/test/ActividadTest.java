1bd046e3703b494da5fa2ee4331b1fe4d22b0674
RobertoCoto <roberto.coto@fygsolutions.com>
2016-10-06 16:54:44 -0500

    se actualiza test

--- MultiSitioNegocio/src/test/java/com/fyg/multisitio/negocio/test/ActividadTest.java
+++ MultiSitioNegocio/src/test/java/com/fyg/multisitio/negocio/test/ActividadTest.java
@@ -1,19 +1,53 @@
 package com.fyg.multisitio.negocio.test;
 
-import static org.junit.Assert.*;
+
 
 import org.junit.Before;
 import org.junit.Test;
 
-public class ActividadTest {
+import com.fyg.multisitio.dto.Actividad;
+import com.fyg.multisitio.negocio.OperacionesMultiSitioNegocio;
+import com.fyg.multisitio.comun.GUIDGenerator;
+import com.fyg.multisitio.comun.LogHandler;
 
+public class ActividadTest {
+	/**
+	 * Se crea el objeto data para mandarle valores a negocio
+	 */
+	private OperacionesMultiSitioNegocio data;
+	/**
+	 *Uid unico generado
+	 */
+	 private GUIDGenerator uid;
+	 /**
+	  * se crea objeto para registrar articulo
+	  */
+	 private Actividad registraActividad;
 	@Before
 	public void setUp() throws Exception {
+		 data = new OperacionesMultiSitioNegocio();
+		 
+		 /**
+		  * se le envian los datos a actividad
+		  */
+		 registraActividad = new Actividad();
+		 registraActividad.setNombre("Nadar");
+		 registraActividad.setDescripcion("Natacion, nadar,agua");
+		 registraActividad.setIdGiro(1);
 	}
 
 	@Test
-	public void testRegistraActividad() {
-		fail("Not yet implemented");
+	@SuppressWarnings("static-access")
+	public void testRegistraActividad() throws Exception {
+		
+		String guid = uid.generateGUID(data);
+		try {
+			data.registraActividad(registraActividad);
+			LogHandler.debug(guid,this.getClass(), "PRUEBA JUNIT");
+		}
+		catch (Exception ex) {
+			LogHandler.debug(guid,this.getClass(), "Error");
+		}
 	}
 
 }
