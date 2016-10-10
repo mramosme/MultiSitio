09b30987e21affb108509cd6e4f55b96b508e37f
RobertoCoto <roberto.coto@fygsolutions.com>
2016-10-10 13:52:37 -0500

    version busqueda JSON

---
+++ MultiSitioWEB/src/main/java/com/fyg/multisitio/web/JSONServiceActividad.java
@@ -0,0 +1,31 @@
+package com.fyg.multisitio.web;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.fyg.multisitio.negocio.ConsultasMultiSitioNegocio;
+import com.fyg.multisitio.dto.Actividad;
+
+@Path("/busqueda")
+public class JSONServiceActividad {
+
+	 @Path("{i}")
+	 @POST
+	 @Produces("application/json")
+	 public  Response jsonActividadBusqueda (@PathParam("i") String f) throws JSONException {
+		 JSONObject jsonObject = new JSONObject();
+		 
+		 String result = "" + jsonObject;
+		 return Response.status(200).entity(result).build();
+	 }
+	
+}
