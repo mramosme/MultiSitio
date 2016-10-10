8383513fa6546ce6bf2ea2ab5cfce60ed98c20bd
RobertoCoto <roberto.coto@fygsolutions.com>
2016-10-10 10:48:35 -0500

    se agrega metodo busqueda

--- MultiSitioNegocio/src/main/java/com/fyg/multisitio/negocio/ConsultasMultiSitioNegocio.java
+++ MultiSitioNegocio/src/main/java/com/fyg/multisitio/negocio/ConsultasMultiSitioNegocio.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.fyg.multisitio.comun.EncabezadoRespuesta;
 import com.fyg.multisitio.comun.GUIDGenerator;
 import com.fyg.multisitio.comun.LogHandler;
+import com.fyg.multisitio.dto.Actividad;
 import com.fyg.multisitio.dto.Articulo;
 import com.fyg.multisitio.dto.FiltroNegocio;
 import com.fyg.multisitio.dto.FiltroSitio;
@@ -97,5 +98,30 @@ public class ConsultasMultiSitioNegocio {
 		    }
 		    LogHandler.debug(uid, this.getClass(), "consultaSitio - Datos Salida: " + respuesta);
 	return listaArticulo;
+	}
+	
+	public List<Actividad> busquedaActividad(Actividad actividad) {
+		//Primero generamos el identificador unico de la transaccion
+				String uid = GUIDGenerator.generateGUID(actividad);
+				//Mandamos a log el objeto de entrada
+				LogHandler.debug(uid, this.getClass(), "Busqueda Actividad - Datos Entrada: " + actividad);
+				//Variable de resultado
+			    EncabezadoRespuesta respuesta = new EncabezadoRespuesta();
+			    respuesta.setUid(uid);
+				respuesta.setEstatus(true);
+				respuesta.setMensajeFuncional("Consulta correcta.");
+			    List<Actividad> listaActividad = null;
+			    try {
+			    	listaActividad = new ConsultaMultiSitio().consultaActividad(uid,actividad);
+				    	
+				    } catch (Exception ex) {
+				    	LogHandler.error(uid, this.getClass(), "ConsultaMultiSitio - ErrorMultisitio: " + ex.getMessage(), ex);
+						respuesta.setUid(uid);
+						respuesta.setEstatus(false);
+						respuesta.setMensajeFuncional(ex.getMessage());
+						respuesta.setMensajeTecnico(ex.getMessage());
+				    }
+				    LogHandler.debug(uid, this.getClass(), "Busqueda Actividad - Datos Salida: " + respuesta);
+			return listaActividad;
 	}
 }
